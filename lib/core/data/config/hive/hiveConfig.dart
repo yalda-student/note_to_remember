abfdbb5a15558fb753501780d8c7933769fccdf7
khoujani <hosseinkhojany1380@gmail.com>
2022-04-28 23:06:56 +0430

    mvvm (repository pattern)

--- lib/core/data/config/hive/hiveConfig.dart
+++ lib/core/data/config/hive/hiveConfig.dart
@@ -2,6 +2,7 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:yalda_students_notes/core/data/datasource/local/hive_db.dart';
 
 const categoryBox = "CATEGORIES_BOX";
+const favoriteNoteBox = "FAVORITE_NOTE_BOX";
 const sharedStore = "SHARED_STORE";
 
 class HiveConfig {
--- lib/core/data/datasource/local/hive_db.dart
+++ lib/core/data/datasource/local/hive_db.dart
@@ -1,5 +1,5 @@
-import 'package:drift/drift.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:yalda_students_notes/core/data/datasource/local/sqlite3_db.dart';
 import 'package:yalda_students_notes/core/data/model/category_model.dart';
@@ -7,46 +7,45 @@ import 'package:yalda_students_notes/core/data/model/note_model.dart';
 
 import '../../config/hive/hiveConfig.dart';
 
-
 class HiveDb {
-
-
   getKey(id) => (id.value ?? 0).toString();
 
   Future<bool> addNote(NoteCompanion noteCompanion) async {
-      try {
-        final box = await Hive.openBox(getKey(noteCompanion.categoryId));
-        int id = DateTime.now().millisecondsSinceEpoch;
-        await box.put(id,
-            NoteModel(
-                id: id,
-                title: noteCompanion.title.value,
-                content: noteCompanion.content.value,
-                createdAt: noteCompanion.createdAt.value,
-                color: noteCompanion.color.value,
-                isFavorite: noteCompanion.isFavorite.value,
-                categoryId: noteCompanion.categoryId.value,
-                category: await getCategoryTitle(noteCompanion.categoryId.value)));
-        return Future.value(true);
-      } catch (e) {
-        debugPrint(e.toString());
-        return Future.value(true);
-      }
+    try {
+      final box = await Hive.openBox(getKey(noteCompanion.categoryId));
+      int id = DateTime.now().millisecondsSinceEpoch;
+      await box.put(
+          id,
+          NoteModel(
+              id: id,
+              title: noteCompanion.title.value,
+              content: noteCompanion.content.value,
+              createdAt: noteCompanion.createdAt.value,
+              color: noteCompanion.color.value,
+              isFavorite: noteCompanion.isFavorite.value,
+              categoryId: noteCompanion.categoryId.value,
+              category:
+                  await getCategoryTitle(noteCompanion.categoryId.value)));
+      return Future.value(true);
+    } catch (e) {
+      debugPrint(e.toString());
+      return Future.value(true);
+    }
   }
 
   Future<bool> deleteNote(int id, int categoryId) async {
-    try{
+    try {
       final box = await Hive.openBox(getKey(categoryId));
       await box.delete(id);
       return Future.value(true);
-    }catch(e){
+    } catch (e) {
       debugPrint(e.toString());
       return Future.value(false);
     }
   }
 
   Future<bool> updateNote(NoteData noteData) async {
-    try{
+    try {
       final box = await Hive.openBox(getKey(noteData.categoryId));
       NoteModel editedData = NoteModel(
           id: noteData.id,
@@ -60,64 +59,109 @@ class HiveDb {
       await box.put(noteData.id, editedData);
 
       return Future.value(true);
-    }catch(e){
+    } catch (e) {
       debugPrint(e.toString());
       return Future.value(false);
     }
-
   }
 
   Future<NoteData> getNoteById(int id) async {
-
-    throw UnimplementedError();
+    final catBox = await Hive.openBox(categoryBox);
+    List<NoteData> result = <NoteData>[];
+    for (var element in catBox.keys) {
+      final box = await Hive.openBox(element);
+      for (var note in box.values) {
+        if(id == (note as NoteModel).id){
+          result.add(NoteData(
+              id: note.id,
+              title: note.title,
+              content: note.content,
+              createdAt: note.createdAt,
+              color: note.color,
+              isFavorite: note.isFavorite,
+              categoryId: note.categoryId));
+        }
+      }
+    }
+    return result[0];
   }
 
-  Stream<List<TypedResult>> getAllNotes({String keyword = ''}) {
-
-    throw UnimplementedError();
+  Future<List<NoteModel>> getAllNotes({String keyword = ''}) async {
+    final catBox = await Hive.openBox(categoryBox);
+    List<NoteModel> result = <NoteModel>[];
+    for (var element in catBox.keys) {
+      final box = await Hive.openBox(element);
+      for (var note in box.values) {
+        result.add(note);
+      }
+    }
+    return result;
   }
 
-  Stream<List<TypedResult>> getStarNotes() {
-
-    throw UnimplementedError();
+  Future<List<NoteModel>> getStarNotes() async {
+    final box = await Hive.openBox(favoriteNoteBox);
+    List<NoteModel> result = <NoteModel>[];
+    for (var element in box.values) {
+      result.add(element);
+    }
+    return result;
   }
 
   Future<List<NoteData>> getNotesInCategory(int categoryId) async {
-
-    throw UnimplementedError();
+    final box = await Hive.openBox(getKey(categoryId));
+    List<NoteData> result = <NoteData>[];
+    for (var element in box.values) {
+      result.add(element);
+    }
+    return result;
   }
 
   Future<void> addCategory(CategoryCompanion categoryCompanion) async {
     final box = Hive.lazyBox(categoryBox);
-    int categoryId = (categoryCompanion.createdAt.value.year.toString()+
-        categoryCompanion.createdAt.value.month.toString()+
-            categoryCompanion.createdAt.value.day.toString()+
-            categoryCompanion.createdAt.value.hour.toString()+
-            categoryCompanion.createdAt.value.minute.toString()+
-            categoryCompanion.createdAt.value.second.toString() as int);
-    box.put(categoryCompanion.createdAt, CategoryModel(id: categoryId, title: categoryCompanion.title.value));
+    int categoryId = (categoryCompanion.createdAt.value.year.toString() +
+        categoryCompanion.createdAt.value.month.toString() +
+        categoryCompanion.createdAt.value.day.toString() +
+        categoryCompanion.createdAt.value.hour.toString() +
+        categoryCompanion.createdAt.value.minute.toString() +
+        categoryCompanion.createdAt.value.second.toString() as int);
+    box.put(categoryCompanion.createdAt,
+        CategoryModel(id: categoryId, title: categoryCompanion.title.value));
   }
 
-  Future deleteCategory(int categoryId) async {
-
-    throw UnimplementedError();
+  Future<bool> deleteCategory(int categoryId) async {
+    try {
+      final box = Hive.lazyBox(categoryBox);
+      box.delete(categoryId);
+      return Future.value(true);
+    } catch (e) {
+      debugPrint(e.toString());
+      return Future.value(false);
+    }
   }
 
-  Future<void> updateCategory(CategoryData categoryData) async {
-
-    throw UnimplementedError();
+  Future<bool> updateCategory(CategoryData categoryData) async {
+    try {
+      final box = Hive.lazyBox(categoryBox);
+      box.put(categoryData.id,
+          CategoryModel(id: categoryData.id, title: categoryData.title));
+      return Future.value(true);
+    } catch (e) {
+      debugPrint(e.toString());
+      return Future.value(false);
+    }
   }
 
-  Stream<List<TypedResult>> getAllCategories() {
-
-    throw UnimplementedError();
+  Future<List<CategoryModel>> getAllCategories() async {
+    final box = await Hive.openBox(categoryBox);
+    List<CategoryModel> result = <CategoryModel>[];
+    for (var element in box.values) {
+      result.add(element);
+    }
+    return Future.value(result);
   }
 
   Future<String> getCategoryTitle(int categoryId) async {
     final box = Hive.lazyBox(categoryBox);
     return Future.value((box.get(categoryId) as CategoryModel).title);
   }
-
-
-
 }
